79276776c6acd2bc845782a969315e94aeea7620
Umberto Toniolo <amountainram@gmail.com>
2024-02-06 08:57:51 +0100

    chore(deps): add `assert_fs` and update tests (#16)

--- tests/client_operations.rs
+++ tests/client_operations.rs
@@ -1,3 +1,4 @@
+use assert_fs::fixture::{FileWriteStr, PathChild};
 use futures::future::join_all;
 use uuid::Uuid;
 
@@ -17,10 +18,10 @@ static TESTED_CLIENTS: [&str; 2] = ["gcs-test", "s3-test"];
 #[tokio::test(flavor = "multi_thread")]
 async fn should_upload_file() {
     // prepare local asset
-    let folder = common::TmpDir::create_tmp_dir();
-    let filename = "file.txt";
-    let src = folder.add_file(filename, "all good 👌!").unwrap();
-    let src = src.to_str().unwrap();
+    let folder = assert_fs::TempDir::new().expect("temp dir to be created");
+    let file = folder.child("file.txt");
+    file.write_str("all good 👌!")
+        .expect("bytes to be written on file");
 
     // pick remote working directory
     let random_folder = Uuid::new_v4().to_string();
@@ -29,7 +30,11 @@ async fn should_upload_file() {
     let dest = url.path();
 
     // prepare remote file urls
-    let filepath = url.end_with_file(filename);
+    let filepath = url.end_with_file(
+        file.file_name()
+            .and_then(|f| f.to_str())
+            .expect("file to have a name"),
+    );
     let filepath = filepath.path();
     let files = [filepath.to_string()];
 
@@ -43,10 +48,16 @@ async fn should_upload_file() {
             .filter(|&(name, _)| TESTED_CLIENTS.contains(&name.as_ref()))
             .map(|(_, client)| async {
                 // upload asset
-                client.upload(src, dest, None).await.unwrap();
+                client
+                    .upload(&file.to_string_lossy(), dest, None)
+                    .await
+                    .unwrap();
 
                 // download asset and assert
-                let bytes = client.download(filepath).await.unwrap();
+                let bytes = client
+                    .download(files.get(0).expect("file to be uploaded").as_str())
+                    .await
+                    .unwrap();
                 assert_eq!(Ok("all good 👌!"), String::from_utf8(bytes).as_deref());
             })
             .collect::<Vec<_>>(),
